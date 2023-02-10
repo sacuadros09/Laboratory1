d86fa16a91d66b38ff0f0986c6d2fef10db768be
Santiago Cuadros Mateus <sacuma09@gmail.com>
2023-02-10 18:43:52 -0500

    lab 1

---
+++ index.js
@@ -0,0 +1,6 @@
+const reverse = require('reverse-geocode')
+
+console.log(reverse.lookup(37.7052591, -172.4980652, 'us'))
+
+var random_name = require('node-random-name');
+console.log(random_name()); // -> "Santiago Cuadros"
